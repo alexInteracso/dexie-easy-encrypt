37dcb7f1415393d73515001d4e61584ab45de67c
Fran Saa <fran.saa@interacso.com>
2023-04-11 09:16:57 +0200

    Fix promise errors

--- index.js
+++ index.js
@@ -27,83 +27,77 @@ const middleware = ({ db, encryption = null, tables = [] }) => {
     console.log('passed into db.on.ready');
     const settingsTable = getEncryptionSettingsTable(db);
     console.log('settingsTable', settingsTable);
-    return Promise.resolve()
-      .then(() => {
-        settingsTable
-          .toCollection()
-          .last()
-          .then(previousSettings => {
-            const previousTables =
-              previousSettings && Array.isArray(previousSettings.tables) ? previousSettings.tables : [];
+    return new Promise((resolve, reject) => {
+      settingsTable
+        .toCollection()
+        .last()
+        .then(previousSettings => {
+          const previousTables =
+            previousSettings && Array.isArray(previousSettings.tables) ? previousSettings.tables : [];
 
-            return Promise.resolve().then(() =>
-              Promise.all(
-                db.tables.map(function(table) {
-                  function setupHooks(table, encryption) {
-                    console.log('table', table);
-                    table.hook('creating', (primKey, obj) => {
-                      encryptObject(table, obj, encryption);
-                    });
-                    table.hook('updating', (modifications, primKey, obj) => {
-                      // do we have any modifications?
-                      const modificationKeys = Object.keys(modifications).filter(
-                        x => x !== constants.ENCRYPTED_DATA_KEY
-                      );
-                      if (modificationKeys.length === 0) {
-                        return undefined;
-                      }
-
-                      // decrypt the original object
-                      const decrypted = decryptObject({ ...obj }, encryption);
-
-                      // merge the modifications into the original object
-                      const updates = {
-                        ...decrypted,
-                        ...modifications,
-                      };
-
-                      // encrypt the new object (must not modify passed params)
-                      // wipe keys to undefined instead of deleting from object, which dexie uses to
-                      // remove them from the modifications object
-
-                      encryptObject(table, updates, encryption, true);
-
-                      return updates;
-                    });
-                    table.hook('reading', obj => {
-                      decryptObject(obj, encryption);
-                    });
-                    console.log('hooks installed for', table.name);
+          Promise.all(
+            db.tables.map(function(table) {
+              function setupHooks(table, encryption) {
+                console.log('table', table);
+                table.hook('creating', (primKey, obj) => {
+                  encryptObject(table, obj, encryption);
+                });
+                table.hook('updating', (modifications, primKey, obj) => {
+                  // do we have any modifications?
+                  const modificationKeys = Object.keys(modifications).filter(x => x !== constants.ENCRYPTED_DATA_KEY);
+                  if (modificationKeys.length === 0) {
+                    return undefined;
                   }
 
-                  const scenario = selectTableScenario(table, tables, previousTables);
-                  switch (scenario) {
-                    case constants.SCENARIO_TABLE_UNENCRYPTED_CHANGE: {
-                      setupHooks(table, encryption);
-                      return;
-                    }
-                    case constants.SCENARIO_TABLE_ENCRYPTED_NO_CHANGE: {
-                      setupHooks(table, encryption);
-                      return;
-                    }
-                  }
-                  return Promise.resolve();
-                })
-              )
-            );
-          })
-          .then(() => settingsTable.clear())
-          .then(() => settingsTable.put({ tables }))
-          .catch(error => {
-            if (error.name === 'NotFoundError') {
-              throw new Error(makeError(constants.ERROR_ENCRYPTION_TABLE_NOT_FOUND));
-            }
-            return Promise.reject(error);
-          });
-      })
-      .then(() => {
-        db.easyEncryptReady = true;
-      });
+                  // decrypt the original object
+                  const decrypted = decryptObject({ ...obj }, encryption);
+
+                  // merge the modifications into the original object
+                  const updates = {
+                    ...decrypted,
+                    ...modifications,
+                  };
+
+                  // encrypt the new object (must not modify passed params)
+                  // wipe keys to undefined instead of deleting from object, which dexie uses to
+                  // remove them from the modifications object
+
+                  encryptObject(table, updates, encryption, true);
+
+                  return updates;
+                });
+                table.hook('reading', obj => {
+                  decryptObject(obj, encryption);
+                });
+                console.log('hooks installed for', table.name);
+              }
+
+              const scenario = selectTableScenario(table, tables, previousTables);
+              switch (scenario) {
+                case constants.SCENARIO_TABLE_UNENCRYPTED_CHANGE: {
+                  setupHooks(table, encryption);
+                  return;
+                }
+                case constants.SCENARIO_TABLE_ENCRYPTED_NO_CHANGE: {
+                  setupHooks(table, encryption);
+                  return;
+                }
+              }
+            })
+          );
+        })
+        .then(() => settingsTable.clear())
+        .then(() => settingsTable.put({ tables }))
+        .then(() => resolve())
+        .catch(error => {
+          if (error.name === 'NotFoundError') {
+            throw new Error(makeError(constants.ERROR_ENCRYPTION_TABLE_NOT_FOUND));
+          }
+          return reject(error);
+        });
+    }).then(() => {
+      db.easyEncryptReady = true;
+    });
   });
 };
 
